fb2c0b893f51d82e2edb13f5bae0b43c7c922afc
Daniel Koopmans <daniel.koopmans@true.nl>
2025-04-14 23:20:12 +0200

    chore: Update configurations to v0.3.11

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,6 +56,7 @@ export default async function Home() {
         Version: {appVersion}
       </footer>
       {/* Optional: Add more sections below */}
+      {/* Trigger rebuild for v0.3.11 */}
     </div>
   );
 }
